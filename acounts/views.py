228f2d635f9f6d9b21b7a9cf5bb3eea95d2fd02e
Qammar <qammarsahu@gmail.com>
2023-02-27 05:13:48 +0500

    Templates Added

--- acounts/views.py
+++ acounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render (request,'acounts/dashboard.html')
+
+def product(request):
+    return render (request,'acounts/product.html')
+
+def customer(request):
+    return render (request,'acounts/customer.html')
